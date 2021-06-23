b607ceeec9267381ce584ed85846931ae6e23043
ShailendraErwin <sverma@erwin.com>
2021-06-23 15:11:42 +0530

    JWT token utility

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 // User model
@@ -51,6 +53,7 @@ router.post(
         d: "retro",
       });
 
+      // create user
       user = new User({
         name,
         email,
@@ -61,11 +64,26 @@ router.post(
       // Encrypt password using bcrypt
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+      //Save user in DB
       await user.save();
 
       // Return JsonWebToken so user gets logged in after registering
 
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecretKey"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");

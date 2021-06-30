af09abb3deb3d1442fa3273900377d885ccf6af8
ShailendraErwin <sverma@erwin.com>
2021-06-30 15:04:26 +0530

    Correcting Github API calls

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -302,14 +302,15 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 router.get("/github/:username", (req, res) => {
   try {
     const options = {
-      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`,
+      uri: encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      ),
       method: "GET",
       headers: {
         "user-agent": "node.js",
         Authorization: `token ${config.get("githubToken")}`,
       },
     };
-    console.log(req.params.username);
     request(options, (error, response, body) => {
       if (error) {
         console.error(error);

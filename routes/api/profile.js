2b95dae2b5dd29be8778ad3b3b9adfc641472552
ShailendraErwin <sverma@erwin.com>
2021-06-30 21:22:11 +0530

    Used octokit package to get Github repos of user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const request = require("request");
+const { request } = require("@octokit/request");
 const config = require("config");
 const router = express.Router();
 const auth = require("../../middleware/auth");
@@ -299,28 +299,25 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 //@route  GET api/profile/github/:username
 //@desc   Get user repo from Github
 //@access Public
-router.get("/github/:username", (req, res) => {
+router.get("/github/:username", async (req, res) => {
   try {
-    const options = {
-      uri: encodeURI(
-        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
-      ),
+    const result = await request({
       method: "GET",
+      url: `https://api.github.com/users/{username}/repos`,
+      per_page: 5,
+      sort: "created",
+      username: `${req.params.username}`,
       headers: {
-        "user-agent": "node.js",
-        Authorization: `token ${config.get("githubToken")}`,
+        authorization: `token ${config.get("githubToken")}`,
       },
-    };
-    request(options, (error, response, body) => {
-      if (error) {
-        console.error(error);
-      }
-      if (response.statusCode !== 200) {
-        res.status(404).json({ msg: "GitHub profile not found" });
-      }
-      console.log(response);
-      res.json(JSON.parse(body));
+      type: "public",
     });
+
+    if (result.status !== 200) {
+      return res.status(404).json({ msg: "GitHub profile not found" });
+    }
+
+    res.json(result.data);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");

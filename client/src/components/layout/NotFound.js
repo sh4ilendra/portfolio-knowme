6de7628db668a19dbd180866a095708d089c6034
ShailendraErwin <sverma@erwin.com>
2021-07-25 15:01:06 +0530

    Not found pages utility

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <h1 className="x-large text-primary">
+        <i className="fas fa-exclamation-triangle"></i> Page not found
+      </h1>
+      <p>
+        <p className="large">Sorry, this page does not exist</p>
+      </p>
+    </Fragment>
+  );
+};
+
+export default NotFound;

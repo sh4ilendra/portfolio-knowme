32688b51b0a961557c28bc50ca93a92e334e7be8
ShailendraErwin <sverma@erwin.com>
2021-07-25 13:18:24 +0530

    Like,unlike and remove post

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,9 @@
-import { GET_POSTS, POST_ERROR } from "../actions/constants";
+import {
+  DELETE_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+} from "../actions/constants";
 
 const initialState = {
   posts: [],
@@ -17,10 +22,24 @@ export default function postreducer(state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
-        posts: payload,
+        error: payload,
+        loading: false,
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
         loading: false,
       };
     default:
